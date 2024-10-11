01304cf61db5741435b05baef57346bbdd54f60d
altugcelilDT <extberkualtugk@d-teknoloji.com.tr>
2024-10-11 12:06:29 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ let package = Package(
             name: "PromisedFuture",
             targets: ["PromisedFutureTarget"]),
     ],
+    dependencies: [
+        // Burada GitHub'daki repo linkini ve versiyon numarasını kullanıyoruz
+        .package(url: "https://github.com/altugcello/PromisedFuture.git", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: "PromisedFutureTarget",
