b8d9c7a0c55a713e30b17da6d220d926fbded293
altugcelilDT <extberkualtugk@d-teknoloji.com.tr>
2024-11-18 02:56:46 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,26 @@
-// swift-tools-version:5.7
+// Package.swift
 import PackageDescription
 
 let package = Package(
     name: "PromisedFuture",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v10),
+        .macOS(.v10_12),
+        .tvOS(.v10),
+        .watchOS(.v3)
     ],
     products: [
         .library(
             name: "PromisedFuture",
-            targets: ["PromisedFutureTarget"]),
-    ],
-    dependencies: [
-        // Burada GitHub'daki repo linkini ve versiyon numarasını kullanıyoruz
-        .package(url: "https://github.com/altugcello/PromisedFuture.git", from: "1.0.0")
+            targets: ["PromisedFuture"]),
     ],
     targets: [
         .target(
-            name: "PromisedFutureTarget",
-            path: "Source")
+            name: "PromisedFuture",
+            path: "Source"),
+        .testTarget(
+            name: "PromisedFutureTests",
+            dependencies: ["PromisedFuture"],
+            path: "Tests"),
     ]
 )
-
